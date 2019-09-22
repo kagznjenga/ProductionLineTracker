002f0e3d7c2dcde4e0efc869bbac22b84f1d00ee
Nickolas Gadomski <spudnick79@gmail.com>
2019-09-21 23:45:04 -0400

    Sprint 1 complete

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,32 +1,65 @@
 package sample;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
 
 /**
  * public class controller.
  */
 public class Controller {
 
-  /**
-   * Fields for class controller.
-   */
-  @FXML
-  private Button btnClick;
+    /**
+     * private class fields
+     */
+    @FXML
+    private Button btn_AddProduct;
 
-  @FXML
-  private ComboBox myComboBox;
+    @FXML
+    private ComboBox<Integer> myCmbBox;
 
-  /**
-   * @param event display method for button click.
-   */
-  @FXML
-  void display(ActionEvent event) {
-    String sql = "INSERT INTO Product(type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' )";
-  }
+    @FXML
+    private Button btn_recordProduction;
+
+    @FXML
+    void chooseQuality(MouseEvent event) {
+
+    }
+
+    /**
+     * @param event Executes an action when a mouse event takes place
+     */
+    @FXML
+    void display(MouseEvent event) {
+        System.out.println("Add Product Clicked");
+    }
+
+    /**
+     * @param event A mouse event
+     */
+    @FXML
+    void recordProduction(MouseEvent event) {
+        System.out.println("Record Production Clicked.");
+    }
+
+    /**
+     * combobox initializer
+     */
+    @FXML
+    public void initialize() {
+        myCmbBox.setEditable(true);
+        myCmbBox.getItems().add(1);
+        myCmbBox.getItems().add(2);
+        myCmbBox.getItems().add(3);
+        myCmbBox.getItems().add(4);
+        myCmbBox.getItems().add(5);
+        myCmbBox.getItems().add(6);
+        myCmbBox.getItems().add(7);
+        myCmbBox.getItems().add(8);
+        myCmbBox.getItems().add(9);
+        myCmbBox.getItems().add(10);
+        myCmbBox.getSelectionModel().selectFirst();
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,35 +6,75 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 /*
- * @author Samuel Njenga.
+ *  * @author Samuel Njenga.
  * @brief This program maintains a database containing information on products and allows
  * various operations to be performed on the data stored.
  * */
 
 public class Main extends Application {
 
-  /**
-   * The start method sets the stage for the GUI program.
-   *
-   * @param primaryStage The main stage where elements are contained
-   * @throws Exception caught by the compiler.
-   */
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("Production Line Tracker");
-    primaryStage.setScene(new Scene(root, 500, 450));
-    primaryStage.show();
+    /**
+     * The start method sets the stage for the GUI program.
+     *
+     * @param primaryStage The main stage where elements are contained
+     * @throws Exception caught by the compiler.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Production Line Tracker");
+        primaryStage.setScene(new Scene(root, 500, 450));
+        primaryStage.show();
 
-  }
+    }
 
-  /**
-   * Main method where program execution begins.
-   *
-   * @param args contains supplied command-line arguments as an array.
-   */
-  public static void main(String[] args) {
-    launch(args);
-  }
+    /**
+     * Main method where program execution begins.
+     *
+     * @param args contains supplied command-line arguments as an array.
+     */
+    public static void main(String[] args) {
+        final String JDBC_DRIVER = "org.h2.Driver";
+        final String DB_URL = "jdbc:h2:./res/ProductDatabase";
+
+
+        final String USER = "";
+        final String PASS = "";
+        Connection conn = null;
+        Statement stmt = null;
+
+        /**
+         * try block for checking connection
+         */
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+            System.out.println("Database Connection Established.");
+
+            stmt.close();
+            conn.close();
+
+            /**
+             * catch block for exception
+             */
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+            /**
+             * catch block for exception
+             */
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        launch(args);
+    }
 }
